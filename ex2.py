74c849501d60139d02cfa46f16df2bcf394dab33
Madelyn Hobbs <madelynhobbs@Madelyns-MacBook-Pro.local>
2016-08-18 21:24:14 -0500

    Initial

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# a comment, this is so you so you can read your program later.
+# anthing after the # is ignored by python.
+
+print("I could have code like this.")
+
+#print "This won't run."
+
+print("This will run.")
