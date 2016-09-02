6c33bb0b4c6f188351cf3912dd58f8dccceca2ce
Madelyn Hobbs <madelynhobbs@Madelyns-MacBook-Pro.local>
2016-09-02 16:53:26 -0500

    Redoing some code

--- ex32.py
+++ ex32.py
@@ -2,11 +2,11 @@ print("What is your name?")
 name = input()
 
 
-age = float(input("Age? "))
+age = float(input('Age?'))
 
 if age > 35:
     print("Time for a tombstone!")
 else:
     print("Have a nice life!")
 
-print("Hello my name is %r and I'm %r years old." % (name, age))
+print("Hello my name is %s, and I'm %d years old." % (name, age))
